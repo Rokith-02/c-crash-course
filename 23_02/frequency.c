18ace3a915d64c29d0eab36119da5211a0a8e988
Rokith <98573941+Rokith-02@users.noreply.github.com>
2022-02-23 17:37:52 +0530

    Update frequency.c

--- 23_02/frequency.c
+++ 23_02/frequency.c
@@ -3,16 +3,16 @@
 void fun(char*a)
 {
         int i,j,count;
-        for(i=0;a[i];i++)
+        for(i=0;a[i];i++)//it will iterate upto null character
         {
-                for(j=0;j<i;j++)
+                for(j=0;j<i;j++)//we have to check up to i. we want to move one after another
                 {
                         if(a[j]==a[i])
                                 break;
                 }
-                if(j==i)
-                {
-                        for(j=j+1,count=1;a[j];j++)
+                if(j==i)//if both(j==i) are pointing to same character means we found that charater is first time we are going to count
+                {                                    //if both are not same that already counted
+                        for(j=j+1,count=1;a[j];j++)//after (j==i) we start to count that char already we found 1 time so count start's with 1.
                         {
                                 if(a[i]==a[j])
                                         count++;
